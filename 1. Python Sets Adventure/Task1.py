a2eccb9bab956cd333fe1072611b34f265ec8ae7
jdanos07 <jdanos07@gmail.com>
2024-10-29 21:50:47 -0400

    completion

--- 1. Python Sets Adventure/Task1.py
+++ 1. Python Sets Adventure/Task1.py
@@ -11,5 +11,21 @@
 
 # Example Code:
 
-# our_routes = {"LAX", "JFK", "CDG", "DXB"}
-# competitor_routes = {"JFK", "CDG", "LHR", "BKK"}
+our_routes = {"LAX", "JFK", "CDG", "DXB"}
+competitor_routes = {"JFK", "CDG", "LHR", "BKK"}
+
+def shared_routes(competitor_routes):
+    common_routes = our_routes.intersection(competitor_routes)
+    print(common_routes)
+
+def unique_routes(competitor_routes):
+    our_unique_routes = our_routes.difference(competitor_routes)
+    print(our_unique_routes)
+
+def exclusive_routes(competitor_routes):
+   unshared_routes = our_routes.symmetric_difference(competitor_routes)
+   print(unshared_routes)
+
+shared_routes(competitor_routes)
+unique_routes(competitor_routes)
+exclusive_routes(competitor_routes)
--- 2. Set Operations in Data Analysis/Task2.py
+++ 2. Set Operations in Data Analysis/Task2.py
@@ -1,10 +1,17 @@
-# Task 1: Duplicate Entries Cleanup You are given a list of customer IDs, some of 
-# which are duplicated. Write a Python script to remove duplicates and display 
-# the unique customer IDs.
+# Task 1: Duplicate Entries Cleanup 
+# You are given a list of customer IDs, some of which are duplicated. 
+# Write a Python script to remove duplicates and display the unique customer IDs.
 
 # Example Code:
 
-# customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
+customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
 
 # Expected Outcome: A set of unique customer IDs, ensuring no duplicates. 
 # For instance, `{'C001', 'C002', 'C003', 'C004'}`. ---
+
+
+def id_cleaner(customer_ids):
+    customer_set = set(customer_ids)
+    print((customer_set))
+
+id_cleaner(customer_ids)
